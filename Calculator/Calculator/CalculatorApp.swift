705e0388c3a73a140d3cfc0045cfc789f027fc0e
Sarah <sarahireneclark@gmail.com>
2025-02-26 16:33:07 -0600

    Created initial Xcode project

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sarah Clark on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
