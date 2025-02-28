8a11ea2daade60b3dd8ac8fa7179557538b1bfac
Sarah <sarahireneclark@gmail.com>
2025-02-28 13:47:33 -0600

    Changed calculator button positions

--- Calculator/Calculator/Views/CalculatorView.swift
+++ Calculator/Calculator/Views/CalculatorView.swift
@@ -12,10 +12,10 @@ struct CalculatorView: View {
     @State private var showingSessionSheet = false
 
     private let buttonLayout: [[ButtonConfig?]] = [
-        [.init(title: "7"), .init(title: "8"), .init(title: "9"), .init(title: "÷")],
-        [.init(title: "4"), .init(title: "5"), .init(title: "6"), .init(title: "×")],
+        [.init(title: "7"), .init(title: "8"), .init(title: "9"), .init(title: "AC")],
+        [.init(title: "4"), .init(title: "5"), .init(title: "6"), .init(title: "+")],
         [.init(title: "1"), .init(title: "2"), .init(title: "3"), .init(title: "−")],
-        [.init(title: "0"), .init(title: "AC"), .init(title: "="), .init(title: "+")]
+        [.init(title: "0"), .init(title: "×"), .init(title: "÷"), .init(title: "=")]
     ]
 
     var body: some View {
